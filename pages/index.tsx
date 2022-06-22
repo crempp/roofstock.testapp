bc2f7c841b14579e7136278e423ee347af0bba56
Chad Rempp <crempp@gmail.com>
2022-06-22 10:42:16 -0700

    trigger ci

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,6 @@ const Home: NextPage = () => {
 
         <p className={styles.description}>
           Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
         </p>
 
         <div className={styles.grid}>
